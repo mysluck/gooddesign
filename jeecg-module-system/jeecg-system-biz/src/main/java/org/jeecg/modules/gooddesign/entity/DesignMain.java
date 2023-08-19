1a17d4b5dfac65979ff79cc91114083ca216c5bc
zhouziyu <zhouziyu@dibgroup.cn>
2023-08-19 20:50:00 +0800

    相关人员

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/gooddesign/entity/DesignMain.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/gooddesign/entity/DesignMain.java
@@ -1,27 +1,24 @@
 package org.jeecg.modules.gooddesign.entity;
 
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.jeecg.common.aspect.annotation.Dict;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description: 设计壮游
  * @Author: jeecg-boot
- * @Date:   2023-08-18
+ * @Date:   2023-08-19
  * @Version: V1.0
  */
 @Data
@@ -32,44 +29,52 @@ import lombok.experimental.Accessors;
 public class DesignMain implements Serializable {
     private static final long serialVersionUID = 1L;
 
-	/**主键*/
-	@TableId(type = IdType.ASSIGN_ID)
+    /**主键*/
+    @TableId(type = IdType.AUTO)
     @ApiModelProperty(value = "主键")
-    private java.lang.String id;
-	/**创建人*/
+    private Integer id;
+    /**创建人*/
     @ApiModelProperty(value = "创建人")
-    private java.lang.String createBy;
-	/**创建日期*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    private String createBy;
+    /**创建日期*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建日期")
-    private java.util.Date createTime;
-	/**更新人*/
+    private Date createTime;
+    /**更新人*/
     @ApiModelProperty(value = "更新人")
-    private java.lang.String updateBy;
-	/**更新日期*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    private String updateBy;
+    /**更新日期*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新日期")
-    private java.util.Date updateTime;
-	/**年份*/
-	@Excel(name = "年份", width = 15)
+    private Date updateTime;
+    /**年份*/
+    @Excel(name = "年份", width = 15)
     @ApiModelProperty(value = "年份")
-    private java.lang.String year;
-	/**城市*/
-	@Excel(name = "城市", width = 15)
+    private String year;
+    /**城市*/
+    @Excel(name = "城市", width = 15)
     @ApiModelProperty(value = "城市")
-    private java.lang.String city;
-	/**标题*/
-	@Excel(name = "标题", width = 15)
+    private String city;
+    /**标题*/
+    @Excel(name = "标题", width = 15)
     @ApiModelProperty(value = "标题")
-    private java.lang.String title;
-	/**介绍*/
-	@Excel(name = "介绍", width = 15)
+    private String title;
+    /**介绍*/
+    @Excel(name = "介绍", width = 15)
     @ApiModelProperty(value = "介绍")
-    private java.lang.String content;
-	/**是否推荐 1推荐 0不推荐*/
-	@Excel(name = "是否推荐 1推荐 0不推荐", width = 15)
+    private String content;
+    /**是否推荐 1推荐 0不推荐*/
+    @Excel(name = "是否推荐 1推荐 0不推荐", width = 15)
     @ApiModelProperty(value = "是否推荐 1推荐 0不推荐")
-    private java.lang.Integer recommendedStatus;
+    private Integer recommendedStatus;
+    /**视频链接*/
+    @Excel(name = "视频链接", width = 15)
+    @ApiModelProperty(value = "视频链接")
+    private String movieUrl;
+    /**封面链接*/
+    @Excel(name = "封面链接", width = 15)
+    @ApiModelProperty(value = "封面链接")
+    private String imgCoverUrl;
 }
