153cdf3b6bb488efc33dc5b752f877864c53ec2a
zhouziyu <zhouziyu@dibgroup.cn>
2023-08-22 09:02:01 +0800

    七牛云获取token

---
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/oss/QiNiuConfiguration.java
@@ -0,0 +1,38 @@
+package org.jeecg.config.oss;
+
+import lombok.Data;
+import org.jeecg.common.util.oss.OssBootUtil;
+import org.jeecg.common.util.qiniu.Auth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 云存储 配置
+ *
+ * @author: jeecg-boot
+ */
+@Data
+@Configuration
+//@ConditionalOnProperty(prefix = "jeecg.qiniu", name = "endpoint")
+@ConfigurationProperties(prefix = "jeecg.qiniu")
+
+public class QiNiuConfiguration {
+
+    @Value("${jeecg.qiniu.accessKey}")
+    private String accessKeyId;
+    @Value("${jeecg.qiniu.secretKey}")
+    private String accessKeySecret;
+    @Value("${jeecg.qiniu.bucketName}")
+    private String bucketName;
+    @Value("${jeecg.qiniu.staticDomain:}")
+    private String staticDomain;
+
+
+    @Bean
+    public void initQiniuBootConfiguration() {
+        Auth.create(accessKeyId, accessKeySecret);
+    }
+}
---
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/gooddesign/controller/QiNiuAuthController.java
@@ -0,0 +1,35 @@
+package org.jeecg.modules.gooddesign.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.api.vo.Result;
+import org.jeecg.common.util.qiniu.Auth;
+import org.jeecg.config.oss.QiNiuConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Description:
+ * @Author: zhouziyu
+ * @Date: 2023/8/22 8:40
+ * @Version: V1.0
+ */
+@Api(tags = "好设计-七牛云-获取TOKEN")
+@RestController
+@RequestMapping("/auth")
+@Slf4j
+public class QiNiuAuthController {
+    @Autowired
+    QiNiuConfiguration qiNiuConfiguration;
+
+    @GetMapping("getToken")
+    public Result getToken() {
+        Auth auth = Auth.create(qiNiuConfiguration.getAccessKeyId(), qiNiuConfiguration.getAccessKeySecret());
+        String design = auth.uploadToken("design");
+        return Result.OK(design);
+
+    }
+}
